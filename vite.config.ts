d2694c4bced70497c63d8401e786e2e5e20f16b0
nanakura <1749094641@qq.com>
2023-08-08 17:19:15 +0800

    magrate

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import solid from 'solid-start/vite'
 import { defineConfig, PluginOption } from 'vite'
 import civetPlugin from 'vite-plugin-civet'
+import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [
     {
       ...civetPlugin({
@@ -15,6 +23,18 @@ export default defineConfig({
     solid({
       extensions: ['.civet'],
       rootEntry: '~/root.civet',
+    }),
+    Unocss({
+      include: ["src/**/*.civet"]
+    }),
+    AutoImport({
+      imports: [
+        'solid-js',
+      ],
+      dts: true,
+      dirs: [
+        'src/primitives'
+      ]
     }),
   ],
 })
